3eb35a7a6aa8090b456f5f003a3d853069fa7ba7
Tomislav Jakopec <tjakopec@gmail.com>
2018-11-19 14:35:48 +0100

    Dorada mala/velika slova

--- src/ffos/Ljubav.java
+++ src/ffos/Ljubav.java
@@ -17,7 +17,7 @@ public class Ljubav {
 	 * @return tipa void pa ne vraća vrijednost.
 	 */
 	public static void main(String[] args) {
-		if (args[0].equals("Marija") && args[1].equals("Zvonko")) {
+		if (args[0].toLowerCase().equals("marija") && args[1].toLowerCase().equals("zvonko")) {
 			System.out.println("Volite se 98 %");
 		}else {
 			System.out.println("Volite se " + Math.random() + " %");
